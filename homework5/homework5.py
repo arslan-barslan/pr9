3431eb8d5e107deeed789b5584b052a6c8ee5d59
ArslanIbragimovPI332b <79196922+arslan-barslan@users.noreply.github.com>
2024-11-30 16:27:10 +0500

    Update homework5.py

--- homework5/homework5.py
+++ homework5/homework5.py
@@ -1,12 +1,10 @@
 import random
 
-# Генерация списка случайных чисел
-n = 10  # Количество элементов в списке
+
+n = 10  
 numbers = [random.randint(1, 100) for _ in range(n)]
 
 print("Список чисел:", numbers)
-
-# Поиск элементов, которые больше предыдущего
 result = [numbers[i] for i in range(1, len(numbers)) if numbers[i] > numbers[i - 1]]
 
 print("Элементы, которые больше предыдущего:", result)
