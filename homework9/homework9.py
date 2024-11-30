1cd1be731acd0d559a13b408cd0ae1c98939bd01
ArslanIbragimovPI332b <79196922+arslan-barslan@users.noreply.github.com>
2024-11-30 16:32:53 +0500

    Update homework9.py

--- homework9/homework9.py
+++ homework9/homework9.py
@@ -1,8 +1,5 @@
 import re
-
-# Функция для разбора email
 def pochtafinder(email):
-    # Регулярное выражение для разбора email
     pattern = r'^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$'
     match = re.match(pattern, email)
     if match:
@@ -11,12 +8,9 @@ def pochtafinder(email):
     else:
         return None, None
 
-# Основная программа
 print("Добро пожаловать в приложение для разбора email!")
 email = input("Введите ваш email: ")
-
 username, domain = pochtafinder(email)
-
 if username and domain:
     print(f"Username: {username}")
     print(f"Domain: {domain}")
