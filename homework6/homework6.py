3c7f2f579a5628464cccc0d0b1a5002cbe0d30eb
ArslanIbragimovPI332b <79196922+arslan-barslan@users.noreply.github.com>
2024-11-30 16:28:26 +0500

    Update homework6.py

--- homework6/homework6.py
+++ homework6/homework6.py
@@ -1,18 +1,16 @@
 import random
 
-# Генерация списка случайных чисел
-n = 10  # Количество элементов в списке
+n = 10  
 numbers = [random.randint(1, 100) for _ in range(n)]
 
 print("Изначальный список:", numbers)
 
-# Поиск минимального и максимального элемента и их индексов
 min_index = numbers.index(min(numbers))
 max_index = numbers.index(max(numbers))
 min_value = numbers[min_index]
 max_value = numbers[max_index]
 
-# Замена местами минимального и максимального элемента
+
 numbers[min_index], numbers[max_index] = numbers[max_index], numbers[min_index]
 
 print("Измененный список:", numbers)
